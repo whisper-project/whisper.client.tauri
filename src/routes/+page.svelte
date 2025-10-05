9e3109320c7956d2fa190c0fa3242bb846e783b9
Daniel Brotsky <dan@brotsky.com>
2025-10-05 13:47:46 -0700

    Reduce main screen padding.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import HomeScreen from '$lib/components/home/HomeScreen.svelte';
 </script>
 
-<div class="min-h-screen bg-gray-200 p-8 flex items-center justify-center">
+<div class="min-h-screen bg-gray-200 p-2.5 flex items-center justify-center">
 	<div class="w-full max-w-5xl h-screen max-h-[90vh] min-h-[600px]">
 		<HomeScreen />
 	</div>
