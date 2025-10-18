68289672e8d779d67a75d59dd66953171896ea13
Daniel Brotsky <dan@brotsky.com>
2025-10-18 10:25:34 -0700

    Update configs

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'node:url';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
@@ -37,5 +37,6 @@ export default defineConfig(
 				svelteConfig
 			}
 		}
-	}
+	},
+	globalIgnores(['./src-tauri/', './build'])
 );
